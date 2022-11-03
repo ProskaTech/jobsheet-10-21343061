e4331661d14368e9416e6b3365646511d8eaaad3
Nazwa Nurfadhilla Phitri <112637218+21343061@users.noreply.github.com>
2022-11-03 19:50:57 +0700

    Add files via upload

---
+++ Prak10/src/main/java/smartphone/AndroMain.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package smartphone;
+
+/**
+ *
+ *  Created by Nazwa Nurfadhilla Phitri
+ */
+public class AndroMain {
+    public static void main(String[] args) {
+    Android HP = new Android();
+    HP.nyala();
+    HP.panggilan();
+    HP.sms();
+    HP.shutdown();
+    }
+}
